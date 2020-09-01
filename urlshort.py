a250b904a18215b3437b26322b562e8aa7a02302
jake7405 <jake7405@gmail.com>
2020-09-01 09:23:53 -0700

    updated file

--- urlshort.py
+++ urlshort.py
@@ -9,3 +9,14 @@ print("Input your credentials below. Credential verification is in progress. Rig
 # user input account credentials
 username = input("Enter username: ")
 password = getpass.getpass("Enter Password: ")
+
+# obtain access token
+auth = requests.post("https://api-ssl.bitly.com/oauth/access_token", auth=(username, password))
+if auth.status_code == 200:
+    # response 200 is a good request
+    access_token = auth.content.decode()    # decode access token
+    print("SUCCESS: access token received")
+else:
+    # response if 200 not received/bad credentials/failed connection
+    print("[!] Could not get access token. Try again.")
+    exit()
